170a4cb64c27b000bc18bbde136f78c4ab943fb5
mimitheone <mgnaskova@yahoo.com>
2024-06-06 19:00:39 +0200

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -1,3 +1,3 @@
 //let first_answer = document.querySelector("#first_anwer");
-first_answer.innerHTML = document.querySelector("#first_anwer") + 4;
+first_answer.innerHTML = document.querySelector("#first_answer") + 4;
 title.innerHTML = "Great1";
